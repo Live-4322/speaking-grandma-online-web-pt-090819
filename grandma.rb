4cc1d916c052c5bb04ffa28fba0572f809bd0291
Charlie Oster <oster.charlie@yahoo.com>
2019-09-15 23:01:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,9 +8,10 @@ elsif phrase == "Hi Nana, how are you?"
   "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "Hi!"
   puts "HUH?! SPEAK UP, SONNY!"
-  
+  "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "WHAT DID YOU EAT TODAY?"
   puts "NO, NOT SINCE 1938!"
+  
 elsif phrase == "WHAT?"
   puts "NO, NOT SINCE 1938!"
 else 
