fb4e77472ebfe4e01b795d71bea0844b481df4fb
Charlie Oster <oster.charlie@yahoo.com>
2019-09-15 23:00:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,10 @@ def speak_to_grandma(phrase)
 
 if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-
+"I LOVE YOU TOO PUMPKIN!"
 elsif phrase == "Hi Nana, how are you?"
   puts "HUH?! SPEAK UP, SONNY!"
+  
 elsif phrase == "Hi!"
   puts "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "WHAT DID YOU EAT TODAY?"
