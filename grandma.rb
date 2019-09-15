4de4ea3e11d4e38593550f0d744e88ac6d2e0abb
Charlie Oster <oster.charlie@yahoo.com>
2019-09-15 22:46:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,25 @@
+def speak_to_grandma = "I LOVE YOU GRANDMA!"
+
+if speak_to_grandma == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+elsif speak_to_grandma == "Hi Nana, how are you?"
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif speak_to_grandma == "Hi!"
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+  puts "NO, NOT SINCE 1938!"
+elsif speak_to_grandma == "WHAT?"
+  puts "NO, NOT SINCE 1938!"
+else 
+  puts "Listen to Grandma talk about the old days"
+end
+
+  
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
